d585324d537909eb7ca90dbc955e29ced7064ec4
Dom Rodriguez <shymega@shymega.org.uk>
2023-05-25 23:47:40 +0100

    fix(flake): Fix Flake to use OpenSSL as input

--- flake.nix
+++ flake.nix
@@ -1,44 +1,34 @@
+# SPDX-FileCopyrightText: Dom Rodriguez <shymega+toybox@shymega.org.uk>
+# SPDX-License-Identifier: GPL-3.0-or-later
 {
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-  inputs.nci.url = "github:yusdacra/nix-cargo-integration";
-  inputs.nci.inputs.nixpkgs.follows = "nixpkgs";
-  inputs.parts.url = "github:hercules-ci/flake-parts";
-  inputs.parts.inputs.nixpkgs-lib.follows = "nixpkgs";
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
 
-  outputs = inputs @ {
-    parts,
-    nci,
-    ...
-  }:
-    parts.lib.mkFlake {inherit inputs;} {
-      systems = ["x86_64-linux"];
-      imports = [nci.flakeModule];
-      perSystem = {
-        pkgs,
-        config,
-        ...
-      }: let
-        # TODO: change this to your crate's name
-        crateName = "abm_git_update_bot";
-        # shorthand for accessing this crate's outputs
-        # you can access crate outputs under `config.nci.outputs.<crate name>` (see documentation)
-        crateOutputs = config.nci.outputs.${crateName};
-      in {
-        # declare projects
-        # relPath is the relative path of a project to the flake root
-        # TODO: change this to your crate's path
-        nci.projects.${crateName}.relPath = "";
-        # configure crates
-        nci.crates.${crateName} = {
-          # export crate (packages and devshell) in flake outputs
-          # alternatively you can access the outputs and export them yourself (see below)
-          export = true;
-          # look at documentation for more options
+  outputs = { self, flake-utils, naersk, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = (import nixpkgs) {
+          inherit system;
         };
-        # export the crate devshell as the default devshell
-        devShells.default = crateOutputs.devShell;
-        # export the release package of the crate as default package
-        packages.default = crateOutputs.packages.release;
-      };
-    };
+
+        naersk' = pkgs.callPackage naersk {};
+
+      in rec {
+        # For `nix build` & `nix run`:
+        defaultPackage = naersk'.buildPackage {
+          src = ./.;
+          nativeBuildInputs = with pkgs; [ pkg-config cmake ] ;
+          buildInputs = with pkgs; [ openssl ];
+        };
+
+        # For `nix develop`:
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [ rustc cargo pkg-config cmake];
+          buildInputs = with pkgs; [ openssl ];
+        };
+      }
+    );
 }
